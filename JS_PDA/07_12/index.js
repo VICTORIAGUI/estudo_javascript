351e047dd28fcb065ffe6f86d6addefcfd6334a1
VICTORIAGUI <victoriagui.santos@gmail.com>
2022-12-07 17:35:31 -0300

    js no pda

---
+++ JS_PDA/07_12/index.js
@@ -0,0 +1,6 @@
+console.log('Hello World!'); // Executa na saida do terminal.
+console.log({Nome: 'Victoria', Cidade: 'Caxias'});
+//alert('Hello Wordl!'); // Executa no console do inspecionar no navegador.
+document.write('Hello Word'); // Executa na pagina do html
+
+// documente.white se usa muito no html/css para mudanças rápidas e dinamicas.
